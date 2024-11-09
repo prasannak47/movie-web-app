347d0f36a02196bd97d958792208dcc51575c6ee
prassanna kulkarni <124696513+prasannak47@users.noreply.github.com>
2024-11-09 17:03:35 +0530

    Update ProfilePage.js

--- Assignment 01-03/ProfilePage.js
+++ Assignment 01-03/ProfilePage.js
@@ -6,8 +6,8 @@ import ProfileDetails from './ProfileDetails';
 const ProfilePage = () => {
     const user = {
         profilePicture: 'https://via.placeholder.com/150', // Replace with your image URL
-        name: 'Harshavardhan Kakade',
-        email: 'Harshk2296@gmail.com',
+        name: 'Prasanna Madan Kulkarni'
+        email: 'KulkarniPrasanna94@gmail.com',
         bio: 'A software developer with a passion for coding and learning new technologies RaectJs.'
     };
 
